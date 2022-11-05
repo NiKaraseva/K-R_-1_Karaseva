3fb8bb1cb7a647cb0d191d2e23c1e52c6798ba3c
Nika <nikarasevas@gmail.com>
2022-11-06 00:26:16 +0300

    Get final array

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -9,10 +9,15 @@ for(int i = 0; i < n; i++)
 } 
 Console.WriteLine();
 Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
 
 int size = SizeArray(array);
 Console.WriteLine($"Размер итогового массива = {size}");
-Console.WriteLine();
+
+
+string[] array2 = NewArray(array, size);
+Console.WriteLine("Итоговый массив:[" + string.Join(", ", array2) + "]");
+
 
 int SizeArray(string[] array)
 {
@@ -27,4 +32,21 @@ int SizeArray(string[] array)
         }
     }
     return size;
+}
+
+string[] NewArray(string[] array, int size)
+{
+    string[] result = new string[size];
+    int length;
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        length = array[i].Length;
+        if(length <= 3)
+        {
+            result[j] = array[i];
+            j++;
+        }           
+        
+    }
+    return result;
 }
