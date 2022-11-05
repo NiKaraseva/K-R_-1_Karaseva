49a95f48a0885c526fe23372ccd91deca19e8514
Nika <nikarasevas@gmail.com>
2022-11-06 00:21:30 +0300

    Create first array

---
+++ Ex_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задайте длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+string[] array = new string[n];
+for(int i = 0; i < n; i++)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = Console.ReadLine()!;
+} 
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
