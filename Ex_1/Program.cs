d2f5098b76a0c293a71c1d4169e854b908b9543a
Nika <nikarasevas@gmail.com>
2022-11-06 00:23:43 +0300

    Get size final array

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -9,3 +9,22 @@ for(int i = 0; i < n; i++)
 } 
 Console.WriteLine();
 Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+int size = SizeArray(array);
+Console.WriteLine($"Размер итогового массива = {size}");
+Console.WriteLine();
+
+int SizeArray(string[] array)
+{
+    int size = 0;
+    int length;
+    for(int i = 0; i < array.Length; i++)
+    {
+        length = array[i].Length;
+        if(length <= 3)
+        {
+            size++;
+        }
+    }
+    return size;
+}
